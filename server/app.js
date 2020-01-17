a9901385bf6ad6244425b870c41e8994b8ccd3d3
michael.rode <michael.rode@rigup.com>
2020-01-16 19:49:27 -0600

    First GraphQL React App

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
+const schema = require('./schema/schema');
+
+const app = express();
+
+mongoose.connect('mongodb://mike:password1@ds263808.mlab.com:63808/gql-ninja');
+mongoose.connection.once('open', () => {
+  console.log('connected to db');
+});
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+app.listen(4000, () => {
+  console.log('now listening for requests on port 4000');
+});
