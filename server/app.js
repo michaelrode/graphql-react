5b688ecbcdd577e789a93241540601e43f7bc429
michael.rode <michael.rode@rigup.com>
2020-01-16 20:27:04 -0600

    Add client and book list

--- server/app.js
+++ server/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const mongoose = require('mongoose');
 const schema = require('./schema/schema');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 mongoose.connect('mongodb://mike:password1@ds263808.mlab.com:63808/gql-ninja');
 mongoose.connection.once('open', () => {
